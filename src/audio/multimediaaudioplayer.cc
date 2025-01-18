0c4ffc1ed7014a101e0c5051dce1274450808ee5
Gzngskxgr20 <74984541+Athameral@users.noreply.github.com>
2025-01-18 12:12:24 +0000

    try to remove output after the play ends.

--- src/audio/multimediaaudioplayer.cc
+++ src/audio/multimediaaudioplayer.cc
@@ -16,11 +16,18 @@ MultimediaAudioPlayer::MultimediaAudioPlayer()
   connect( &player, &QMediaPlayer::errorChanged, this, &MultimediaAudioPlayer::onMediaPlayerError );
 
   connect( &mediaDevices, &QMediaDevices::audioOutputsChanged, this, &MultimediaAudioPlayer::audioOutputChange );
+
+  connect( &player, &QMediaPlayer::mediaStatusChanged, this, &MultimediaAudioPlayer::onPlayEnds);
 }
 
 void MultimediaAudioPlayer::audioOutputChange()
 {
   qDebug() << "audio device changed";
+}
+
+void MultimediaAudioPlayer::onPlayEnds()
+{
+  player.setAudioOutput( nullptr );
 }
 
 QString MultimediaAudioPlayer::play( const char * data, int size )
--- src/audio/multimediaaudioplayer.hh
+++ src/audio/multimediaaudioplayer.hh
@@ -25,6 +25,7 @@ public:
 private slots:
   void onMediaPlayerError();
   void audioOutputChange();
+  void onPlayEnds();
 
 
 private:
